6bc65cf7a3c71c0ff60ef8c3713966610cf651bd
Thiago de Bastos <thiago@thiagodebastos.com>
2016-06-27 22:43:42 +1000

    use array instead of object, add comments

--- source/js/main.js
+++ source/js/main.js
@@ -1,35 +1,47 @@
+// cache ids
 const quoteButton = document.getElementById('quoteButton');
-const quoteBoxContent = $('#quotebox--quote-js');
-const quoteBoxAuthor = $('#quotebox--author-js');
+const quoteBoxContent = document.getElementById('quotebox--quote-js');
+const quoteBoxAuthor = document.getElementById('quotebox--author-js');
+
 // Revealing module pattern
 const getQuote = (() => {
 
+  // create a random number to be used when retriving quote index
   const random = (min, max) => {
     const x = Math.floor(Math.random() * (max - min)) + min;
     return x;
   };
+  /*
+  NOTE: One option was to use an object, but this left double-quotes
+  in the results. I removed the quotes with .match(/\"/g, "")
+  but supposedly regex slows down performance.
+  ===========================================================
+  const quote = {
+    author: "Krishnamurti",
+    quote: "You are the world and the world is you.",
+  };
 
-  const quote = new Object();
+  // put in $.getJSON callback:
+  // from: http://stackoverflow.com/a/14389539/3108412
+  quote.author = data.quotes[num].author;
+  quote.quote = data.quotes[num].quote;
+  quoteBoxContent.text(JSON.stringify(data.quotes[num].quote));
+  quoteBoxAuthor.text(JSON.stringify(data.quotes[num].author));
+  ===========================================================*/
 
-    function fetchQuote() {
-      $.getJSON('/data/quotes.json', (data) => {
-        const num = random(0, data.quotes.length);
-        quote.author = data.quotes[num].author;
-        quote.quote = data.quotes[num].quote;
-        quoteBoxContent.html(JSON.stringify(quote.quote));
-        quoteBoxAuthor.html(JSON.stringify(quote.author));
-      });
-      quoteBoxContent.text(JSON.stringify(quote.quote));
-      quoteBoxAuthor.text(JSON.stringify(quote.author));
-    };
-    console.log(JSON.stringify(quote.author));
+  function fetchQuote() {
+    const quote = [];
+    $.getJSON('/data/quotes.json', (data) => {
+      const num = random(0, data.quotes.length);
+      quote.push(data.quotes);
+      $(quoteBoxContent).text(quote[0][num].quote);
+      $(quoteBoxAuthor).text(quote[0][num].author);
+    });
+  };
 
   // explicitly return public methods when this object is instantiated
   return {
     quote: fetchQuote,
-  }
-
+  };
 })();
 quoteButton.addEventListener('click', getQuote.quote);
-//  example usage
-// getQuote.fetch(2, 10); // alerts "my method"
